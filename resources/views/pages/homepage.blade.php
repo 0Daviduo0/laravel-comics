510546f0548f03606ef54ffe3fbe24535ec73657
Daviduo <davide0400@gmail.com>
2023-01-30 22:59:53 +0100

    sub_footer done

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -310,4 +310,192 @@
 
     </div>
 
+
+
+    <footer class="d-flex flex-column justify-content-center text-light py-5">
+
+
+        <div class="colums_wrapper d-flex">
+
+            <div class="column column1 d-flex flex-column">
+
+                <div class="dc_comics">
+    
+                    <div class="title_colums">
+                        DC COMICS
+                    </div>
+    
+                    <div class="column_list d-flex flex-column">
+
+                        <a href="">Characters</a>
+    
+                    
+                        <a href="">Comics</a>
+    
+                    
+                        <a href="">Movies</a>
+
+                    
+                        <a href="">TV</a>
+
+                    
+                        <a href="">Games</a>
+
+                    
+                        <a href="">Videos</a>
+
+                    
+                        <a href="">News</a>
+
+                    </div>
+    
+    
+                </div>
+    
+                <div class="shop">
+    
+                    <div class="title_colums">
+                        SHOP
+                    </div>
+    
+                    <div class="column_list d-flex flex-column">
+
+                        <a href="">Shop DC</a>
+    
+                    
+                        <a href="">Shop DC Collectibles</a>
+
+                    </div>
+    
+                </div>
+    
+            </div>
+    
+            <div class="column column2">
+    
+                <div class="dc">
+    
+                    <div class="title_colums">
+                        DC
+                    </div>
+    
+                    <div class="column_list d-flex flex-column">
+
+                        <a href="">Terms Of Use</a>
+    
+                    
+                        <a href="">Privacy Policy (New)</a>
+    
+                    
+                        <a href="">Ad Choices</a>
+
+                    
+                        <a href="">Advertising</a>
+
+                    
+                        <a href="">Jobs</a>
+
+                    
+                        <a href="">Subscriptions</a>
+
+                    
+                        <a href="">Talent Workshops</a>
+
+
+                        <a href="">CPSC Certificates</a>
+        
+                        
+                        <a href="">Ratings</a>
+        
+                        
+                        <a href="">Shop Help</a>
+    
+                        
+                        <a href="">Contact Us</a>
+
+                    </div>
+    
+                </div>
+                
+            </div>
+    
+            <div class="column column3">
+    
+                <div class="sites">
+    
+                    <div class="title_colums">
+                        SITES
+                    </div>
+    
+                    <div class="column_list d-flex flex-column">
+
+                        <a href="">DC</a>
+    
+                    
+                        <a href="">MAD Magazine</a>
+    
+                    
+                        <a href="">DC Kids</a>
+
+                    
+                        <a href="">DC Universe</a>
+
+                    
+                        <a href="">DC Power Visa</a>
+
+                    </div>
+    
+                </div>
+                
+            </div>
+
+        </div>
+
+        <div class="copyright_cookies">
+
+            <div class="copyright">
+                All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="">noted here</a>. All right reserved.
+            </div>
+            <div class="cookie">
+                <a href="">Cookie settings</a> 
+            </div>
+
+        </div>
+
+        <div class="dc_big_logo">
+            <img src="{{ Vite::asset('/resources/img/dc-logo-bg.png') }}" alt="">
+        </div>
+
+    </footer>
+
+
+
+    <div class="sub_footer_social d-flex justify-content-between align-items-center py-1">
+        <div class="signin my-5 py-2 px-4 text-light">
+            SIGN-UP NOW!
+        </div>
+        
+        <div class="socials d-flex align-items-center py-5">
+            <div class="socials_text pe-4">
+                FOLLOW US
+            </div>
+            <div class="socials_group d-flex">
+                <div class="social pe-3">
+                    <img src="{{ Vite::asset('/resources/img/footer-facebook.png') }}" alt="">
+                </div>
+                <div class="social pe-3">
+                    <img src="{{ Vite::asset('/resources/img/footer-twitter.png') }}" alt="">
+                </div>
+                <div class="social pe-3">
+                    <img src="{{ Vite::asset('/resources/img/footer-youtube.png') }}" alt="">
+                </div>
+                <div class="social pe-3">
+                    <img src="{{ Vite::asset('/resources/img/footer-pinterest.png') }}" alt="">
+                </div>
+                <div class="social pe-3">
+                    <img src="{{ Vite::asset('/resources/img/footer-periscope.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
