d907e5ff3e2a00b7d279e457d401090af2c16a5b
Daviduo <davide0400@gmail.com>
2023-01-30 17:18:39 +0100

    base + upload img

---
+++ resources/views/pages/details.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main-layout')
+
+@section('main')
+    <h1>Dettagli prodotto</h1>
+@endsection
---
+++ resources/views/pages/homepage.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main-layout')
+
+@section('main')
+    <h1>DC Comics - Homepage</h1>
+@endsection
